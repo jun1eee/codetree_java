d708082443b3cfd010997d968338909c6fc19597
jun1eee <162537692+jun1eee@users.noreply.github.com>
2025-08-20 17:11:09 +0900

    Title: 그래프 탐색, Time: 442ms, Memory: 24MB, Status: Passed - Codetree

--- 250820/그래프 탐색/graph-traversal.java
+++ 250820/그래프 탐색/graph-traversal.java
@@ -1,5 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
+    public static List<Integer>[] g;
+    public static boolean[] visited;
+    public static int cnt = 0;
+
+    public static void dfs(int n){
+        for (int num : g[n]){
+            if (!visited[num]){
+                visited[num] = true;
+                cnt++;
+                dfs(num);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -10,7 +24,22 @@ public class Main {
             u[i] = sc.nextInt();
             v[i] = sc.nextInt();
         }
-        
+
+        g = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++){
+            g[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++){
+            g[u[i]].add(v[i]);
+            g[v[i]].add(u[i]);
+        }
+
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1);
+
+        System.out.println(cnt);
         
     }
 }
