41f8235bb557a24aa9f3e997dd2b9f651755ccd1
jun1eee <162537692+jun1eee@users.noreply.github.com>
2025-08-20 17:00:35 +0900

    Title: 그래프 탐색, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250820/그래프 탐색/graph-traversal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] u = new int[m];
+        int[] v = new int[m];
+        for (int i = 0; i < m; i++) {
+            u[i] = sc.nextInt();
+            v[i] = sc.nextInt();
+        }
+        
+        
+    }
+}
