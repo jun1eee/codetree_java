b6a25f7a440b406209258516cd7064acdacda460
jun1eee <162537692+jun1eee@users.noreply.github.com>
2025-08-26 09:34:29 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 1249ms, Memory: 24MB, Status: Passed - Codetree

--- 250826/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 250826/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -2,30 +2,51 @@ import java.util.*;
 
 public class Main {
     static int N, K;
-    static int[] arr;
+    static ArrayList<Integer> nums = new ArrayList<>();
+
+    static void print(){
+        for (int i=0; i < nums.size(); i++){
+            System.out.print(nums.get(i) + " ");
+        }
+        System.out.println();
+    }
+    
+    static void f(int cnt) {
+        if (cnt == N) {
+            print();
+            return;
+        }
+
+        for (int i = 1; i <= K; i++) {
+            nums.add(i);
+            f(cnt+1);
+            nums.remove(nums.size()-1);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         K = sc.nextInt();  // 1 ~ K
         N = sc.nextInt();  // 길이 N
-        arr = new int[N];
+        // arr = new int[N];
 
-        dfs(0);
+        f(0);
+        // dfs(0);
     }
 
     // 깊이 우선 탐색으로 수열 만들기
-    static void dfs(int depth) {
-        if (depth == N) {
-            for (int i = 0; i < N; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
+    // static void dfs(int depth) {
+    //     if (depth == N) {
+    //         for (int i = 0; i < N; i++) {
+    //             System.out.print(arr[i] + " ");
+    //         }
+    //         System.out.println();
+    //         return;
+    //     }
 
-        for (int i = 1; i <= K; i++) {
-            arr[depth] = i;
-            dfs(depth + 1);
-        }
-    }
+    //     for (int i = 1; i <= K; i++) {
+    //         arr[depth] = i;
+    //         dfs(depth + 1);
+    //     }
+    // }
 }
