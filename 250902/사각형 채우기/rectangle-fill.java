a2f10703a693401d7b2ebc45d928677d55b21210
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-02 10:22:46 +0900

    Title: 사각형 채우기, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/사각형 채우기/rectangle-fill.java
+++ 250902/사각형 채우기/rectangle-fill.java
@@ -4,14 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] dp = new int[n+1];
-        if (n==1){
-            System.out.println(1);
-            return;
-        }
-        if (n==2){
-            System.out.println(2);
-            return;
-        }
+        
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i<=n; i++){
