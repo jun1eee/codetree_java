e2b9aa1fc882452bfb4ff4314bfce17eccc040c3
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-28 23:09:29 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 156ms, Memory: 12MB, Status: Passed - Codetree

--- 250928/정수 사각형 최댓값의 최소/minimax-path-in-square.java
+++ 250928/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -15,20 +15,19 @@ public class Main {
 		int[][] dp = new int[N][N];
 		dp[0][0] = arr[0][0];
 		for (int i = 1; i < N; i++) {
-			dp[i][0] = Math.max(arr[i][0], arr[i-1][0]);
+			dp[i][0] = Math.max(arr[i][0], dp[i-1][0]);
 		}
 		for (int j = 1; j < N; j++) {
-			dp[0][j] = Math.max(arr[0][j], arr[0][j-1]);
+			dp[0][j] = Math.max(arr[0][j], dp[0][j-1]);
 		}
 		for (int i = 1; i < N; i++) {
 			for (int j = 1; j < N; j++) {
-				int up = Math.max(dp[i-1][j], arr[i][j]);
-				int down = Math.max(dp[i][j-1], arr[i][j]);
-				dp[i][j] = Math.min(up, down);
+				int fromUp   = Math.max(dp[i-1][j], arr[i][j]);
+                int fromLeft = Math.max(dp[i][j-1], arr[i][j]);
+                dp[i][j] = Math.min(fromUp, fromLeft);
 			}
 		}
+		
 		System.out.println(dp[N-1][N-1]);
-		
-		
 	}
 }
